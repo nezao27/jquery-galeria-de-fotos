4ba89d59be0ad3683b1e446f71e5d6e40fc30d17
jonas <jonasrenno2004@gmail.com>
2025-07-18 10:43:59 -0300

    Correção de ortografia da palavra append no código JS

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-$(document).ready(function(){ //O $ chama o jQuery//
+$(document).ready(function() { //O $ chama o jQuery//
     $('header button').click(function() {
         $('form').slideDown();
     })
 
-    $('botao-cancelar').click(function() {
+    $('#botao-cancelar').click(function() {
         $('form').slideUp();
     })
 
     $('form').on('submit', function(e) {
         e.preventDefault();
-        const enderecoNovaImagem = $('endereco-imagem-nova').val();
+        const enderecoNovaImagem = $('#endereco-imagem-nova').val();
         const novoItem = $('<li style="display: none"></li>');
-        $(`<img src="${enderecoNovaImagem}" />`).apendTo(novoItem);
+        $(`<img src="${enderecoNovaImagem}" />`).appendTo(novoItem);
         $(`<div class="overlay-imagem-link">
                 <a href="${enderecoNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
-                    Ver imagem em tamanhov real
+                    Ver imagem em tamanho real
                 </a>
-            </div>`).apendTo(novoItem);
-        $(novoItem).apendTo('ul');
+            </div>`).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
         $(novoItem).fadeIn(1000);
-        $('endereco-imagem-nova').val('');
+        $('#endereco-imagem-nova').val('');
     })
 })
